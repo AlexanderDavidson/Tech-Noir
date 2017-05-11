d10bc8c87311f2254231e6029e85d417518e3cae
Alexander Davidson <alexander.harlan.davidson@gmail.com>
2017-05-11 11:36:54 -0700

    linked js and added d3 selectors for objects

---
+++ client/index.js
@@ -0,0 +1,19 @@
+console.log("hello world!?");
+
+// sScene Selector
+var scene = d3.select("a-scene")
+
+//  Object Selectors
+var akira = d3.select("akira")
+
+var district9 = d3.select("district-9")
+
+var minorityReport = d3.select("minority-report")
+
+var twelveMonkeys = d3.select("twelve-monkeys")
+
+var metropolis = d3.select("metropolis")
+
+var designBlimp = d3.select("design-blimp")
+
+var techVK = d3.select("tech-vk")
